000943cebd1da3c66117e575452a19c7d98fb9a5
Dmitry Evgenov <leospb85@gmail.com>
2018-09-07 14:01:11 +0300

    edit 07.09.2018  +1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,6 +15,7 @@ public class Main {
     public static void main(String[] args) throws SQLException, IOException {
 
         //New comment
+        //New comment 1
         SkillView skillView = new SkillView();
         skillView.createTable();
         AccountView accountView = new AccountView();
