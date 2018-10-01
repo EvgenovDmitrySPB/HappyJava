f2094a775d582ec70bd9f42c6943e75e5c604e5d
EvgenovDmitrySPB <13622aA!>
2018-10-01 19:52:03 +0300

    Сервлеты стали доступны по аннотации

--- src/main/java/net/proselyte/crud/controller/servlets/AccountServlet.java
+++ src/main/java/net/proselyte/crud/controller/servlets/AccountServlet.java
@@ -1,13 +1,14 @@
 package net.proselyte.crud.controller.servlets;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-//@WebServlet("/account")
+@WebServlet("/account")
 public class AccountServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
