bb9c35be9a9f87f306f203dbac38998dab286093
EvgenovDS <13622aA!>
2018-11-08 14:21:14 +0300

    spring security и spring MVC

--- src/main/java/net/proselyte/crud/controller/dispatcher/SecurityController.java
+++ src/main/java/net/proselyte/crud/controller/dispatcher/SecurityController.java
@@ -3,18 +3,18 @@ package net.proselyte.crud.controller.dispatcher;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.portlet.ModelAndView;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by EvgenovDS on 07.11.2018.
  */
 @Controller
-@RequestMapping("/")
 public class SecurityController {
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public ModelAndView main(){
+
         return new ModelAndView("index");
-        // Запрос по точке входа "/" будет выводить нам index.jsp, а найдет он его благодаря нашему dispatcher-servlet
+        // Запрос по точке входа "/" будет выводить нам index2.jsp, а найдет он его благодаря нашему dispatcher-servlet
     }
 }
