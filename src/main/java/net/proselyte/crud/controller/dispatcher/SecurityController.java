b81cd18f7bae9f6ba143e8a4dedecaaddaab8e8d
EvgenovDS <13622aA!>
2018-11-08 11:13:49 +0300

    сменил название

--- src/main/java/net/proselyte/crud/controller/dispatcher/SecurityController.java
+++ src/main/java/net/proselyte/crud/controller/dispatcher/SecurityController.java
@@ -1,6 +1,6 @@
 package net.proselyte.crud.controller.dispatcher;
 
-
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.portlet.ModelAndView;
@@ -8,9 +8,11 @@ import org.springframework.web.portlet.ModelAndView;
 /**
  * Created by EvgenovDS on 07.11.2018.
  */
-@org.springframework.stereotype.Controller
-public class Controller {
-    @RequestMapping(value="/", method = RequestMethod.GET)
+@Controller
+@RequestMapping("/")
+public class SecurityController {
+
+    @RequestMapping(method = RequestMethod.GET)
     public ModelAndView main(){
         return new ModelAndView("index");
         // Запрос по точке входа "/" будет выводить нам index.jsp, а найдет он его благодаря нашему dispatcher-servlet
